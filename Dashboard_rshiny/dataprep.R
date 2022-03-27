d876f7853f551bc9d1eebd9ac36748501c5b82f3
shvien <shvien@ucdavis.edu>
2022-03-26 17:17:18 -0700

    rearranged data table options and moved data cleaning

--- Dashboard_rshiny/dataprep.R
+++ Dashboard_rshiny/dataprep.R
@@ -1,9 +1,22 @@
+library(readr) 
+library(lubridate)
+
 #Data Prep
-clean_entire <- read.csv("~/Desktop/ARS_DataAnalytics/Data/clean_entire.csv")
+urlfile<-'https://raw.githubusercontent.com/riddhi0101/ARS_DataAnalytics/main/Dashboard_rshiny/popup_salesF21.csv'
+clean_entire<-read_csv(url(urlfile))
+clean_entire <- clean_entire[complete.cases(clean_entire),]
 
 #First Page: 
 
 #Second Page
+#Adding day of the week column to data set
+date_fact <- as.factor(clean_entire$Date)
+new_format<-strptime(date_fact,format="%m/%d/%y")
+new_date<-as.Date(new_format,format="%Y-%m-%d")
+
+clean_entire$New_Date <- new_date
+clean_entire$Day <- wday(clean_entire$New_Date,label=TRUE)
+clean_entire$Price <- as.numeric(round(parse_number(clean_entire$`Total Price`),0))
 
 #Third Page 
 
